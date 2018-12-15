ad9a65197057a6ebec0e3298f5f445c6f3ae9bc2
z <x@y.com>
2018-12-14 18:10:49 -0800

    More work on dcrdata

--- startdcrdata.sh
+++ startdcrdata.sh
@@ -1,12 +1,9 @@
 cd ../dcrdata
 
-if [[ "" == "$3" ]] ; then
-	echo "Takes three arguments - username, password, and dcrd hostname:port e.g."
-	echo "startdcrdata.sh rpcuser rpcpass localhost:19108"
+if [[ "" == "$2" ]] ; then
+	echo "Takes two arguments - username and password, e.g."
+	echo "   startdcrdata.sh rpcuser rpcpass"
 	exit 1
 fi
 
-if [[ 
-
-./dcrdata --pg --testnet --dcrduser=$1 --dcrdpass=$2 --dcrdserv=$3
-
+./dcrdata --pg --testnet --dcrduser=$1 --dcrdpass=$2
